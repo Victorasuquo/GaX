8c14e988e10b08c3ef98e3155b6856429d23f829
Victorasuquo <jacobasuquo199@email.com>
2025-05-08 17:40:37 +0100

    removed a file

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Install Python dependencies
 pip install -r banking/requirements.txt
 
+# Add the project directory to PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:/opt/render/project/src
+
 # Collect static files
 cd banking
 python manage.py collectstatic --no-input
